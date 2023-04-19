1e6a5b4d0e3f0d2c20a76b42c2f4c918c10faa90
Fierce-Cat <hyx5020@gmail.com>
2023-04-18 20:59:33 -0700

    fix(temp): disable extra aws config

--- LocalSettings.php
+++ LocalSettings.php
@@ -231,15 +231,15 @@ $wgDefaultUserOptions['watchdefault'] = 0;
 // $wgShowDebug = true;
 
 #调用英文站图片
-// $wgForeignFileRepos[] = [
-// 	'class' => ForeignAPIRepo::class,
-// 	'name' => 'StarCitizenTools', // Must be a distinct name
-// 	'apibase' => 'https://starcitizen.tools/api.php',
-// 	'hashLevels' => 2,
-// 	'fetchDescription' => false, // Optional
-// 	'descriptionCacheExpiry' => 0, // 12 hours, optional (values are seconds)
-// 	'apiThumbCacheExpiry' => 0, // 24 hours, optional, but required for local thumb caching
-// ];
+$wgForeignFileRepos[] = [
+	'class' => ForeignAPIRepo::class,
+	'name' => 'StarCitizenTools', // Must be a distinct name
+	'apibase' => 'https://starcitizen.tools/api.php',
+	'hashLevels' => 2,
+	'fetchDescription' => false, // Optional
+	'descriptionCacheExpiry' => 0, // 12 hours, optional (values are seconds)
+	'apiThumbCacheExpiry' => 0, // 24 hours, optional, but required for local thumb caching
+];
 
 #AWS插件 | Cloudflare R2 储存桶配置
 // $wgAWSCredentials = [
@@ -248,16 +248,16 @@ $wgDefaultUserOptions['watchdefault'] = 0;
 // 	'token' => false
 // ];
 
-$wgAWSRegion = $_ENV["S3Region"]; # Northern Virginia
+// $wgAWSRegion = $_ENV["S3Region"]; # Northern Virginia
 
 // Replace <something> with the name of your S3 bucket, e.g. wonderfulbali234.
-$wgAWSBucketName = $_ENV["S3BucketName"];
-$wgAWSBucketDomain = $_ENV["S3BucketDomain"];
-$wgFileBackends['s3']['endpoint'] = $_ENV["S3Endpoint"];
-$wgAWSRepoHashLevels = '2';
+// $wgAWSBucketName = $_ENV["S3BucketName"];
+// $wgAWSBucketDomain = $_ENV["S3BucketDomain"];
+// $wgFileBackends['s3']['endpoint'] = $_ENV["S3Endpoint"];
+// $wgAWSRepoHashLevels = '2';
 
 // If you anticipate using several hundred buckets, one per wiki, then it's probably better to use one bucket
 // with the top level subdirectory as the wiki's name, and permissions properly configured of course.
 // While there are no more performance losses by using such a scheme, it might make things messy. Hence, it's
 // still a good idea to use one bucket per wiki unless you are approaching your 1,000 bucket per account limit.
-$wgAWSBucketTopSubdirectory = "/"; # leading slash is required
+// $wgAWSBucketTopSubdirectory = "/"; # leading slash is required
