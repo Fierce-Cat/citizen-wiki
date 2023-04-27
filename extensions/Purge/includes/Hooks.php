b9e70d75c065a907453ebc90ad1a994e9dd3c82b
LeonRay <123786505+LeonRay297@users.noreply.github.com>
2023-04-27 11:32:35 +0800

    Add files via upload

--- extensions/Purge/includes/Hooks.php
+++ extensions/Purge/includes/Hooks.php
@@ -4,14 +4,15 @@ namespace MediaWiki\Extension\Purge;
 
 use SkinTemplate;
 
-class Hooks implements \MediaWiki\Hook\SkinTemplateNavigation__UniversalHook {
+class Hooks {
 
 	/**
 	 * @link https://www.mediawiki.org/wiki/Manual:Hooks/SkinTemplateNavigation
 	 * @param SkinTemplate $sktemplate The skin template object.
-	 * @param array &$links The existing structured navigation links.
+	 * @param array $links The existing structured navigation links.
+	 * @return bool
 	 */
-	public function onSkinTemplateNavigation__Universal( $sktemplate, &$links ): void {
+	public static function onSkinTemplateNavigation( SkinTemplate &$sktemplate, array &$links ) { 
 		// Use getRelevantTitle if present so that this will work on some special pages
 		$title = method_exists( $sktemplate, 'getRelevantTitle' )
 			? $sktemplate->getRelevantTitle()
@@ -22,9 +23,11 @@ class Hooks implements \MediaWiki\Hook\SkinTemplateNavigation__UniversalHook {
 
 			$links['actions']['purge'] = [
 				'class' => $action === 'purge' ? 'selected' : false,
-				'text' => $sktemplate->msg( 'purge' )->text(),
-				'href' => '#'
+				'text' => wfMessage( 'purge' )->text(),
+				'href' => $title->getLocalUrl( 'action=purge' )
 			];
 		}
+
+		return true;
 	}
 }
