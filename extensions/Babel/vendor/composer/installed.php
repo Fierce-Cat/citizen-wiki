8172b3ed60ff31b130d4923afa38ea7d966492a6
LeonRay <123786505+LeonRay297@users.noreply.github.com>
2023-04-28 08:14:07 +0800

    Add files via upload

---
+++ extensions/Babel/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a6046f473cf070706f883168607a43eb485141d5',
+    'name' => 'mediawiki/babel',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+    ),
+    'mediawiki/babel' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a6046f473cf070706f883168607a43eb485141d5',
+    ),
+  ),
+);
