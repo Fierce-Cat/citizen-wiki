d94a1a6600f0146d15628b38874b9da5fef2311e
Fierce-Cat <hyx5020@gmail.com>
2023-04-28 23:26:20 -0700

    disable wgResponsiveImages

--- LocalSettings.php
+++ LocalSettings.php
@@ -307,3 +307,4 @@ $wgAWSRepoHashLevels = '2';
 // While there are no more performance losses by using such a scheme, it might make things messy. Hence, it's
 // still a good idea to use one bucket per wiki unless you are approaching your 1,000 bucket per account limit.
 $wgAWSBucketTopSubdirectory = ""; # leading slash is required
+$wgResponsiveImages = false;
